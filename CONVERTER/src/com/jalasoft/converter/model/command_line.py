360eaba6cfc93526aa7e799e9e0e1fb35f3682d5
celestepalet <celestepalet@gmail.com>
2022-12-09 21:23:28 -0300

    add empty files

---
+++ CONVERTER/src/com/jalasoft/converter/model/command_line.py
@@ -0,0 +1,26 @@
+#
+# @command_line.py Copyright (c) 2022 Jalasoft.
+# 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+#
+# All rights reserved.
+#
+# This software is the confidential and proprietary information of
+# Jalasoft, ("Confidential Information"). You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with Jalasoft.
+#
+
+import subprocess
+import os
+
+#executes commands in the terminal
+class Command:
+    def __init__(self, cmd):
+        self.cmd = cmd
+
+    #executes commands
+    def run_cmd(self):
+        return subprocess.Popen(self.cmd)
+
+
