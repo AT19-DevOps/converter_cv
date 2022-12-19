7acaf21e6ef5a2351f61bdb8700d7bf39b2a8da5
Rocio Morales <roncio.93@gmail.com>
2022-12-19 16:46:08 -0300

    :hammer: divide image_to_text_converter in two modules, image_to_text_converter and image_to_pdf. Move them to image package

--- CONVERTER/src/com/jalasoft/converter/model/image/image_to_pdf.py
+++ CONVERTER/src/com/jalasoft/converter/model/image/image_to_pdf.py
@@ -1,5 +1,5 @@
 #
-# @image_to_text_converter.py Copyright (c) 2021 Jalasoft.
+# @image_to_pdf.py Copyright (c) 2021 Jalasoft.
 # 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
 # # All rights reserved.
 #
@@ -12,18 +12,6 @@
 
 
 from model.converter import Converter
-
-
-class ImageToTextConvert(Converter):
-    """ Inherits Converter criteria"""
-    def __init__(self, input_file, output_file, lang):
-        super().__init__(input_file, output_file)
-        self.lang = lang
-
-    def convert(self) -> list:
-        """Converts an image with text in a text file"""
-        command_line = ['tesseract', f'{self.input_file}', f'{self.output_file}', '-l', self.lang]
-        return " ".join(command_line)
 
 
 class ImageToPDFConvert(Converter):
