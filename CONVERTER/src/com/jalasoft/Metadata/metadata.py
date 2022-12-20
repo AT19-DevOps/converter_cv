76ccc5d1e29c06d427eda5986802dd572bb6e9b1
dia777 <Selmi.Murana@jala.university>
2022-12-20 18:28:49 -0400

    I am adding the metadata

---
+++ CONVERTER/src/com/jalasoft/Metadata/metadata.py
@@ -0,0 +1,51 @@
+#
+# @image_to_images.py Copyright (c) 2021 Jalasoft.
+# 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+# # All rights reserved.
+#
+# This software is the confidential and proprietary information of
+# Jalasoft, ("Confidential Information"). You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with Jalasoft.
+#
+
+import subprocess
+import os
+import json
+
+class ExifTool(object):
+
+    sentinel = "{ready}\r\n"
+
+    def __init__(self, executable="exiftool(-k).exe"):
+        self.executable = executable
+        self.process = subprocess.Popen(
+            [self.executable, "-stay_open", "True",  "-@", "-"],
+            universal_newlines=True,
+            stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.process.stdin.write("-stay_open\nFalse\n")
+        self.process.stdin.flush()
+
+    def execute(self, *args):
+        args = args + ("-execute\n",)
+        self.process.stdin.write(str.join("\n", args))
+        self.process.stdin.flush()
+        output = ""
+        fd = self.process.stdout.fileno()
+        while not output.endswith(self.sentinel):
+            output += os.read(fd, 4096).decode('utf-8')
+        return output[:-len(self.sentinel)]
+
+    def get_metadata(self, *filenames):
+        return json.loads(self.execute("-G", "-j", "-n", *filenames))
+
+# ##Testing ExifTool
+# exiftooll = ExifTool()
+# metadata =  exiftooll.get_metadata("avion.jpg")
+
+# print(str(metadata[0]))
+
+# tag = metadata
