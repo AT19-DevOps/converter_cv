20b6d41c64e21b7dc43b833a890f42dd080b33a0
dia777 <Selmi.Murana@jala.university>
2022-12-20 17:23:26 -0400

    Moving file and fix comments

--- CONVERTER/src/com/jalasoft/common/logger/Test_Loger.py
+++ CONVERTER/src/com/jalasoft/common/logger/Test_Loger.py
@@ -1,3 +1,15 @@
+#
+# @image_to_images.py Copyright (c) 2021 Jalasoft.
+# 2643 Av Melchor Perez de Olguin, Colquiri Sud, Cochabamba, Bolivia.
+# # All rights reserved.
+#
+# This software is the confidential and proprietary information of
+# Jalasoft, ("Confidential Information"). You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with Jalasoft.
+#
+
 from flask import Flask
 import json
 from flask_cors import CORS
@@ -11,6 +23,7 @@ logger = LoggerManager()
 
 @app.route("/")
 def hello_world():
+    """It is a test of how it is being used"""
     logger.debug("Starting method")
     try:
         logger.info("Starting the operation ")
