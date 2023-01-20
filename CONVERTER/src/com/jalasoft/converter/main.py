b2352125a44b709a6882d1090af097b1bb491d20
MartinJAA <serverus15>
2023-01-20 11:04:03 -0400

    Solve conflicts

--- CONVERTER/src/com/jalasoft/converter/main.py
+++ CONVERTER/src/com/jalasoft/converter/main.py
@@ -58,4 +58,4 @@ api.add_resource(GetMetadata, "/metadatageter")
 
 print(os.getcwd())
 if __name__ == '__main__':
-    app.run(debug = True, port = 5000, host = '127.0.0.1')
+    app.run(debug=True, host='0.0.0.0', port=5000)
